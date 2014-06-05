62bed0716764cbbb0aac18898692502eacc6e69b
ClaasJG <claas.j.gramann@gmail.com>
2014-06-05 16:10:20 +0200

    Made the code more readable

--- RayTracer1/src/c/j/g/ray1/Sphere.java
+++ RayTracer1/src/c/j/g/ray1/Sphere.java
@@ -9,27 +9,27 @@ import static java.lang.Math.*;
 @Data
 @Builder
 public class Sphere {
-	private final Color color;
-	private final Vec3 origin;
-	private final double radius;
+    private final Color color;
+    private final Vec3 origin;
+    private final double radius;
 
-	public HitInfo getNearestIntersection(Ray ray) {
-		Vec3 d = ray.getDirection();
-		Vec3 o = sub(ray.getOrigin(), origin);
-		// coefficients
-		double a = dot(d, d), b = 2 * dot(d, o), c = dot(o, o)
-				- (radius * radius);
-		// discriminant
-		double dis = b * b - 4 * a * c;
-		if (dis < 0)// No intersection
-			return null;
-		double t = (-b - sqrt(dis)) / (2 * a);// QE, we need just a - no +.
-		
-		Vec3 pos = ray.getAt(t);
-		HitInfoBuilder hjb = HitInfo.builder();
-		hjb.distance(t);
-		hjb.gHitPoint(pos);
-		hjb.geo(this);
-		return hjb.build();
-	}
+    public HitInfo getNearestIntersection(Ray ray) {
+	Vec3 d = ray.getDirection();
+	Vec3 o = sub(ray.getOrigin(), origin);
+	// coefficients
+	double a = dot(d, d);
+	double b = 2 * dot(d, o), c = dot(o, o) - (radius * radius);
+	// discriminant
+	double dis = b * b - 4 * a * c;
+	if (dis < 0)// No intersection
+	    return null;
+	double t = (-b - sqrt(dis)) / (2 * a);// QE, we need just a - no +.
+
+	Vec3 pos = ray.getAt(t);
+	HitInfoBuilder hjb = HitInfo.builder();
+	hjb.distance(t);
+	hjb.gHitPoint(pos);
+	hjb.geo(this);
+	return hjb.build();
+    }
 }
