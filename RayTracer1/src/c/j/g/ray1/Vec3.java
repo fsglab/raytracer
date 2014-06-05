d76c580ca2d1db4314fb0a8ded35db0133999a1d
ClaasJG <claas.j.gramann@gmail.com>
2014-06-05 13:58:55 +0200

    Fixed the add methode

--- RayTracer1/src/c/j/g/ray1/Vec3.java
+++ RayTracer1/src/c/j/g/ray1/Vec3.java
@@ -6,42 +6,46 @@ import lombok.experimental.Builder;
 @Data
 @Builder
 public class Vec3 {
-	public static final Vec3 O = new Vec3(0, 0, 0);
-	private final double x, y, z;
+    public static final Vec3 O = new Vec3(0, 0, 0);
+    private final double x, y, z;
 
-	public static double lenSqu(Vec3 v) {
-		return v.x * v.x + v.y * v.y + v.z * v.z;
-	}
+    public static double lenSqu(Vec3 v) {
+	return v.x * v.x + v.y * v.y + v.z * v.z;
+    }
 
-	public static Vec3 sub(Vec3 a, Vec3 b) {
-		Vec3Builder v = Vec3.builder();
-		v.x = a.x - b.x;
-		v.y = a.y - b.y;
-		v.z = a.z - b.z;
-		return v.build();
-	}
+    public static Vec3 sub(Vec3 a, Vec3 b) {
+	Vec3Builder v = Vec3.builder();
+	v.x = a.x - b.x;
+	v.y = a.y - b.y;
+	v.z = a.z - b.z;
+	return v.build();
+    }
 
-	public static Vec3 mul(Vec3 v, double t) {
-		return new Vec3(v.x * t, v.y * t, v.z * t);
-	}
-	
-	public static Vec3 add(Vec3 a, Vec3 b){
-		return a;
-	}
+    public static Vec3 mul(Vec3 v, double t) {
+	return new Vec3(v.x * t, v.y * t, v.z * t);
+    }
 
-	public static double dot(Vec3 a, Vec3 b) {
-		return a.x * b.x + a.y * b.y + a.z * b.z;
-	}
+    public static Vec3 add(Vec3 a, Vec3 b) {
+	Vec3Builder v = Vec3.builder();
+	v.x = a.x + b.x;
+	v.y = a.y + b.y;
+	v.z = a.z + b.z;
+	return v.build();
+    }
 
-	public static final Vec3 nor(Vec3 v) {
-		double len = lenSqu(v);
-		if (len == 0)
-			return v;
-		len = Math.sqrt(len);
-		Vec3Builder n = Vec3.builder();
-		n.x = v.x / len;
-		n.y = v.y / len;
-		n.z = v.z / len;
-		return n.build();
-	}
+    public static double dot(Vec3 a, Vec3 b) {
+	return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+
+    public static final Vec3 nor(Vec3 v) {
+	double len = lenSqu(v);
+	if (len == 0)
+	    return v;
+	len = Math.sqrt(len);
+	Vec3Builder n = Vec3.builder();
+	n.x = v.x / len;
+	n.y = v.y / len;
+	n.z = v.z / len;
+	return n.build();
+    }
 }
